e3f83306aab1fc192662be4be3741c1f95793bf7
gaffOverflow <gaffar.ibrahim18@gmail.com>
2022-12-19 18:21:47 -0800

    Nav function added

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-document.getElementById("textarea").innerHTML="this is a textarea"
+document.getElementById("textarea").innerHTML = "this is a textarea";
+const navigation = document.querySelector(".nav-link");
+const navToggle = document.querySelector("button");
+
+navToggle.addEventListener("click", () => {
+  const show = navigation.getAttribute("data-visible");
+
+  if (show === "false") {
+    navigation.setAttribute("data-visible", true);
+    navigation.setAttribute("aria-expanded", false);
+  } else if (show === "true") {
+    navigation.setAttribute("data-visible", false);
+    navigation.setAttribute("aria-expanded", true);
+  }
+});
